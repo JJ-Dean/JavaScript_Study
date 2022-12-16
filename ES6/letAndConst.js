82f6a19695404136655dc400dac62115c8e13f76
Dean <yjj052800@gmail.com>
2022-12-16 18:22:52 +0900

    let과 const 비교

---
+++ ES6/letAndConst.js
@@ -0,0 +1,7 @@
+let name = '유준재';
+name = 'Dean';
+console.log(name); // let으로 정의된 변수는 새로운 값을 다시 할당할 수 있다.
+
+const name2 = '유준재';
+name2 = 'Dean';
+console.log(name2); // const로 정의된 변수는 새로운 값을 다시 할당할 수 없다.
